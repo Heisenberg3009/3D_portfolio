3ae3c8f80315c9e94c9b9ddd7a8dae35331d534e
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2024-11-15 11:36:33 +0530

    Eleventh commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,18 +31,16 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+    emailjs .send(
+        'service_n3j3iv7', 'template_zc1odry',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Tulsi Narayanan",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "tulsinarayanan33@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'uIIdYZndwcfcz1YGo'
       )
       .then(
         () => {
